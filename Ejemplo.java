8b48a2148c2e22ca101b648b88eb6ddeb1b1a8a6
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-04-28 17:04:29 -0500

    Circulos ejemplo 2 tema 6

--- Ejemplo.java
+++ Ejemplo.java
@@ -5,6 +5,13 @@ public class Ejemplo {
 
     public static void main(String[] args) {
 
-        Circle circulo = new Circle();
+        Circle circulo_A = new Circle();
+        Circle circulo_B = new Circle();
+        Circle circulo_C = circulo_A;
+
+        System.out.println(circulo_A.equals(circulo_B));
+        System.out.println(circulo_B.equals(circulo_C));
+        System.out.println(circulo_C.equals(circulo_A));
+
     }
 }
