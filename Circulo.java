c9c46fe6605fc0379533fab3aa7f71f1f48f4335
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-04-29 11:17:57 -0500

    Ejemplo 2.3 Circulo

---
+++ Circulo.java
@@ -0,0 +1,20 @@
+
+public class Circulo {
+        public double radio;
+        //Esttablecido en centimetros.
+    }
+    public class Example {
+        public static void main(String[] args) {
+            Circle circulo_A = new Circle();
+            circulo_A.radio = 10.0;
+
+            Circle circulo_B = new Circle();
+            circulo_B.radio = 5.0;
+
+            System.out.println(circulo_A.radio);
+            System.out.println(circulo_B.radio);
+
+        }
+
+
+}
