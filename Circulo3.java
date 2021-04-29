85fb6e3159bba4130a937e065b636b4c5ea5417b
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-04-29 11:36:51 -0500

    Ejemplo 2.5 Circulo

---
+++ Circulo3.java
@@ -0,0 +1,21 @@
+public class Circulo3 {
+        public static double PI = 3.141592;
+        public double radio;
+
+
+    public class Example {
+        public static void main(String[] args) {
+            Circle circulo_A = new Circle();
+            circulo_A.PI = 10.0;
+
+            Circle circulo_B = new Circle();
+
+            System.out.println(circulo_A.PI);
+            System.out.println(circulo_B.PI);
+
+        }
+
+
+    }
+
+}
