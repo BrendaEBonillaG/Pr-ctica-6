77d17c95014f8396d3bc8e96dbdfb86c1c0958fa
Brenda Bonilla <brendaelizabethbonillag17.cb22@dgeti.sems.gob.mx>
2021-04-29 10:58:25 -0500

    Ejemplo 2 Variables

---
+++ Ejemplo2.java
@@ -0,0 +1,18 @@
+public class Ejemplo2 {
+    byte valor =10;
+
+    public short numero_corto = 100;
+
+    public int edad = 10;
+
+    private long numero_largo = 234567890;
+    
+    private float numero_pi = 3.1416f;
+
+    protected char letra_a = 'a';
+
+    double numero_euler = 2.7182818284504535360;
+
+    boolean valor_logico = false;
+
+}
